593736be1952d7ef343f252c25d336a6c3a8c284
BHAVANA SAHU <bvasahu@gmail.com>
2025-12-02 14:32:02 +0530

    Add files via upload

---
+++ StringS.py
@@ -0,0 +1,21 @@
+S=input()
+count=0  
+s=" " 
+if "Egypt" in S:
+    count=count+1
+elif "EGYPT" in S:
+    count=count+1
+elif "egypt" in S:
+    count=count+1
+elif   "eGyPt" in S:
+    count=count+1
+
+print(count)
+    
+
+
+        
+        
+        
+        
+        
