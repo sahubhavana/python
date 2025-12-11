e34e886e9a1522bc4f6bdeb26b155497b968caad
BHAVANA SAHU <bvasahu@gmail.com>
2025-12-11 21:01:17 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,56 @@
+##### String ########
+# mystring='Hello'
+# print(mystring)
+
+# mystring="Hello"
+# print(mystring)
+# mystring=''''Hello'''
+# print(mystring)
+
+
+mystring="Global"
+# print(mystring[0])
+# print(mystring[-1])
+# print(mystring[2:5])
+# #print(mystring[9])
+
+# mystring[4]='t'
+# print(mystring)
+
+# del(mystring)
+# print(mystring)
+# str2="The Best"
+# print(mystring+str2)
+# print(mystring*3)
+# count=0
+# for l in "Global the best":
+#     if l=='l':
+#         count=count+1
+# print(count)
+# print('Gl'in "Global the best")
+# print('v'in "Global the best")
+# print("hello".upper())
+# print("Python is the best language in programming language".split())
+# print(''.join(['This is a programming language']))
+# print("Good Morning".find("Mo"))
+# s1="Bad morning"
+# s2=s1.replace("Bad","Good")
+# print(s1)
+# print(s2)
+# s1=input("enetr your string")
+# s2=s1[::-1]
+# if(s2==s1):
+#     print("palindrome")
+# else:
+#     print("not")
+
+##2
+s="my collage name is global"
+l=s.split( )
+l.sort()
+
+s1=" "
+for i in l:
+    s1=s1+i
+print(s1,'\n')
+    
