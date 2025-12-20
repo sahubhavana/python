ad57c3f3c896ffd1da59731f7a3bcd92bd4840be
BHAVANA SAHU <bvasahu@gmail.com>
2025-12-20 23:20:49 +0530

    Add files via upload

--- Husseinsandstring.py
+++ Husseinsandstring.py
@@ -1,5 +1,16 @@
-a=input().strip()
-b=input().strip()
+a=list(input().split())
+b=list(input().split())
 j=0
+l=[]
 if len(a)!=len(b):
+    print("NO")
+for i in range(len(a)):
+    if a[i]!=b[i]:
+        l.append(i)
+if len(l)==2:
+    for i in l:
+        a[i]=b[i]
+if (a==b):
+    print("YES")
+else:
     print("NO")
