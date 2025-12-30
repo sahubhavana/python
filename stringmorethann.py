37f948601b2d7daa2b3b2f58fe2fec106bc60394
BHAVANA SAHU <bvasahu@gmail.com>
2025-12-30 20:51:41 +0530

    Add string length filter script

---
+++ stringmorethann.py
@@ -0,0 +1,8 @@
+n=int(input("enter a length  of word"))
+# l1=[]
+# l=['The','quick','lazy','fox','jump','over','the','lazy','dog']
+# for i in l:
+#     if len(i)>n:
+#         l1.append(i)
+
+# print(l1)      
