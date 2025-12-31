4c5cb4865520b39653e67e9006d4930cb0a29bd5
BHAVANA SAHU <bvasahu@gmail.com>
2025-12-31 10:31:18 +0530

    Create maximum.py

---
+++ maximum.py
@@ -0,0 +1,9 @@
+################################# write a python to find maximum elemnt in a list#####################
+# l=[10,-9,8,4,5,6,7,8,9,-0,-3]
+# max1=0 
+# for i in range(len(l)-1):
+#     if l[i]>max1:
+#         max1=l[i]
+# print("MAximum element of list", max1)
+# x=max(l)
+# print("maximum element of list=",x)
