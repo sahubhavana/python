0ee8d480314864b8ac88cb24b3efc647abfbf13e
BHAVANA SAHU <bvasahu@gmail.com>
2026-01-03 19:41:57 +0530

    Implement dictionary combination program
    Add a program to combine two dictionaries by summing values for common keys.

---
+++ combine.py
@@ -0,0 +1,8 @@
+#Write a Python program to combine two dictionary by adding values for common keys.
+
+# d1 = {'a': 100, 'b': 200, 'c':300}
+# d2 = {'a': 300, 'b': 200, 'd':400}
+# d3={}
+# for key in d1.keys() | d2.keys():
+#     d3[key] = d1.get(key, 0) + d2.get(key, 0)
+# print(d3)
