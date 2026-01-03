2b935ee99ec65e5a9a37434d3ab7203f502b06b9
BHAVANA SAHU <bvasahu@gmail.com>
2026-01-03 19:24:00 +0530

    Create checkdict.py

---
+++ checkdict.py
@@ -0,0 +1,7 @@
+#Write a Python program to check if a dictionary is empty or not.
+        
+# d={}
+# if(d=={}):
+#     print("Dictionary is empty")
+# else:
+#     print("Dictionary is not empty")
