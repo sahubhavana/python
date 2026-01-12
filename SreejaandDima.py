081b7af30241b0011473058eda6a20f1957360b3
BHAVANA SAHU <bvasahu@gmail.com>
2026-01-12 06:55:16 +0530

    Add files via upload

--- SreejaandDima.py
+++ SreejaandDima.py
@@ -1,10 +1,22 @@
 n = int(input())
 l = list(map(int, input().split()))
-l.sort(reverse=True)
-
-sumS = sum(l[i] for i in range(0, n, 2))
-sumD = sum(l[i] for i in range(1, n, 2))
-
-print(sumS, sumD)
+l.sort()
+i=0 
+j=n-1
+turn=0
+ssum=0 
+dsum=0 
+for i in range(n):          
+    if(i%2==0):
+        ssum=ssum+l[j]
+     
+    else:
+        dsum=dsum+l[j]
+     
+    j=j-1
+ 
+print(ssum,dsum)
+        
+    
     
     
