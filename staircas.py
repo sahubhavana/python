a5a0f88a30eb266121f6bf27dbe33e7d32133c3d
BHAVANA SAHU <bvasahu@gmail.com>
2026-01-19 15:43:02 +0530

    Create staircas.py

---
+++ staircas.py
@@ -0,0 +1,12 @@
+ef staircase(n):
+    
+    for i in range(1,n+1):
+        for k in range(n-i):
+            print(" ",end="")
+            
+        for j in range(i):
+            print("#",end="")
+        print()
+        
+n=int(input())
+staircase(n)
