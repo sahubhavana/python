493bbd4dcf6efe0ed2c94f663923b84acac74110
BHAVANA SAHU <bvasahu@gmail.com>
2026-02-24 22:08:01 +0530

    Add simple addition program in Python

---
+++ Addition.py
@@ -0,0 +1,14 @@
+# Simple Python program
+
+# take input from user
+name = input("Enter your name: ")
+
+# print output
+print("Hello,", name, "! Welcome to Python.")
+
+# simple calculation
+a = 10
+b = 5
+sum = a + b
+
+print("Sum of", a, "and", b, "is", sum)
