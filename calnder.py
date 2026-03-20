517ea362ceef8d423c9e0618011b7d597f67f0f8
BHAVANA SAHU <bvasahu@gmail.com>
2026-03-20 21:08:29 +0530

    Create calnder.py

---
+++ calnder.py
@@ -0,0 +1,6 @@
+import calendar
+# To take month and year input from the user
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+# display the calendar
+print(calendar.month(yy, mm))
