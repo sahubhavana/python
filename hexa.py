d9d14f9a31ac83ce032127a1296485c51a14c599
BHAVANA SAHU <bvasahu@gmail.com>
2026-03-20 21:07:04 +0530

    Uncomment code to display number conversions

---
+++ hexa.py
@@ -0,0 +1,6 @@
+#  binary ,octal ,hexadecimal of a number 
+deci=int(input("enter a decimal number"))
+print("Binary of decimal number is",bin(deci))
+print("octal of decimal number is ",oct(deci))
+print("hexadecimal of decimal number is ",hex(deci))
+
