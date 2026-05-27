07f1d2f0de7d5ad3bf31f57d1c0d8c1934e7a397
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-27 23:36:14 +0530

    Create representkilo.py

---
+++ representkilo.py
@@ -0,0 +1,8 @@
+##21.program to represent a file in kilobyte
+##import os
+##file_path = r"C:\Users\DELL\Documents\output.txt"
+### get file size in bytes
+##size_in_bytes = os.path.getsize(file_path)
+### convert into kilobytes
+##size_in_kb = size_in_bytes / 1024
+##print("File Size in KB:", size_in_kb)
