4e3d846b3ed64b15d0a8d8a2df4578558290e889
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-27 23:41:08 +0530

    Create longestword.py

---
+++ longestword.py
@@ -0,0 +1,10 @@
+##16.find a longest word in a file
+
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"r")
+##x=f1.read().split()
+##lword=""
+##for i in x:
+##    if(len(i)>len(lword)):
+##        lword=i
+##        
+##print("largest word of file=",lword)
