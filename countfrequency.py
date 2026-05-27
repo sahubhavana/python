be0cb22be13793e7a63099a80fca7bceb9e9c0d6
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-27 23:40:33 +0530

    Create countfrequency.py

---
+++ countfrequency.py
@@ -0,0 +1,9 @@
+## 17.count frequency of every letter in a word
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"r")
+##d={}
+##x=f1.read().lower()
+##for i in x:
+##    if(i.isalpha()):
+##       d[i]=d.get(i,0)+1
+##for j ,k in d.items():
+##       print(f"{j}:{k}")
