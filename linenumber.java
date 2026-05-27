60dd2f2481cfb8a4ffe77a73639beb523259e1aa
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-27 23:39:37 +0530

    Create linenumber.java

---
+++ linenumber.java
@@ -0,0 +1,10 @@
+## 18.Search word and print matching line number
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"r")
+##x=f1.readlines()
+##a=input("enter a word")
+##for index, i in enumerate(x):
+##    l=i.split()
+##    if a in l:
+##        print("Word found at line index:", index+1)
+##
+##f1.close()
