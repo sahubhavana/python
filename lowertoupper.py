b87718e2e71fd5c442724d45dfa7e81a1bf2344f
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-27 23:38:31 +0530

    Create lowertoupper.py

---
+++ lowertoupper.py
@@ -0,0 +1,9 @@
+## 19.to convert a word from lower to upper and upper to lower
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"r")
+##x=f1.read()
+##swapped=x.swapcase()
+##f2=open(r'C:\Users\DELL\Documents\output1.txt',"w")
+##f2.write(swapped)
+##f1.close()
+##f2.close()
+##f1.close()
