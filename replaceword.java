88fffd0c07e5d61c7a89e5fc170103502d3e652d
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-27 23:37:47 +0530

    Create replaceword.java

---
+++ replaceword.java
@@ -0,0 +1,7 @@
+## 20.to replace a occurence of word with a new word
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"r")
+##x=f1.read()
+##content=x.replace("Python","Java")
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"w")
+##f1.write(content)
+##f1.close()
