bd3b6a50f37d4e8cac0fd7b9624fe23f9483df7b
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-30 09:00:19 +0530

    Create mergetwofile.py

---
+++ mergetwofile.py
@@ -0,0 +1,12 @@
+## 26.Merge Two Files into One
+##Problem Statement: Write a Python program to read the contents of two
+
+##f1=open(r'C:\Users\DELL\Documents\output.txt',"r")
+##f2=open(r'C:\Users\DELL\Documents\output1.txt',"r")
+##f3=open(r'C:\Users\DELL\Documents\output2.txt',"w")
+##x=f1.read()
+##x2=f2.read()
+##f3.write(x)
+##f3.write(x2)
+##f3.close()
+##print("data added succesfully")
