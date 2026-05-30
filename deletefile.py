4ea277f4503d1ed1208d4286ac6b7ecd8800e472
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-30 09:00:55 +0530

    Create deletefile.py

---
+++ deletefile.py
@@ -0,0 +1,10 @@
+## 25.Write a Python program to delete a specified file from disk,
+##with a check to confirm the file exists before attempting deletion.
+
+##import os
+##
+##if os.path.exists(r'C:\Users\DELL\studentdata.txt'):
+##    os.remove(r'C:\Users\DELL\studentdata.txt')
+##    print("file deleted successfully")
+##else:
+##    print("file does not exist")
