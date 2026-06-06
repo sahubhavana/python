e6a6d6d843b3bf272ced204c2686746f0e02011b
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-06 22:29:34 +0530

    Create multilevel.py

---
+++ multilevel.py
@@ -0,0 +1,22 @@
+## multilevel inheritence
+
+
+##class grandfather:
+##    @staticmethod
+##    def dis():
+##        print("i father of father")
+##class father(grandfather):
+##    @staticmethod
+##
+##    def dis1():
+##        print("I father of my son")
+##class son(father):
+##    @staticmethod
+##
+##    def dis2():
+##        print(" I am son of my father and grandson of garnadfather")
+##s=son()
+##s.dis()
+##s.dis1()
+##s.dis2()
+
