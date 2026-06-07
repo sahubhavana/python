8b388c139693845c8119de8edcbad6f60a4175f5
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-07 21:24:19 +0530

    Create mathfloor.py

---
+++ mathfloor.py
@@ -0,0 +1,8 @@
+## floor(): it return a lower value but not less than given value
+
+
+print(math.floor(-4.5))
+print(math.floor(4.5))
+print(math.floor(4))
+print(math.floor(0))
+print(math.floor(-1))
