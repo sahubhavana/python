7259f8e2f29f2f5088a6141cbd7aca424d05f2ce
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-07 21:21:13 +0530

    Create question2.py

---
+++ question2.py
@@ -0,0 +1,18 @@
+##class employe:
+##     def __init__(self,role,dept,salary):
+##        self.role=role
+##        self.dept=dept
+##        self.salary=salary
+##
+##     def showdetail(self):
+##        print("role=",self.role)
+##        print("dept=",self.dept)
+##        print("salary",self.salary)
+##class engineer(employe):
+##    def __init__(self,name,age):
+##        self.name=name
+##        self.age=age
+##        super().__init__("Engineer","AIML",90000)
+##e=engineer("Elon musk",40)
+##e.showdetail()
+##
