012d43fe685a9d153668049a132f41684c84c8e8
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-07 21:25:45 +0530

    Create mathpow.java

---
+++ mathpow.java
@@ -0,0 +1,8 @@
+## pow(x,y): it can alway return a x raised to power of y
+
+
+##print(math.pow(2,3))
+##print(math.pow(2,1))
+##print(math.pow(3,0))
+##print(math.pow(3,-1))
+##print(math.pow(3,2.0))
