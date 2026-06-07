b2cb4a5fb489c0fb87dc97b8d830a22caee66c14
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-07 21:21:53 +0530

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,11 @@
+## question 1:
+
+##class circle:
+##   def Area(self,radius):
+##       print("area of circle=",radius*radius)
+##    def perimeter(self,radius):
+##       print("Perimrter of circle=",2*3.14*radius)
+##        
+##c=circle()
+##c.Area(2)
+##c.perimeter(2)
