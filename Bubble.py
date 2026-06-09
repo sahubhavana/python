87ae83f830e19b8e3d98d3fd5b560db57176762b
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-09 19:16:58 +0530

    Create Bubble.py

---
+++ Bubble.py
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4, 5]
+n=arr.length
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted Array:", arr)
