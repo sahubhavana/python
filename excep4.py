52355bad34c0c808bbb54f2ad71bdb36fcb300f9
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-15 23:00:45 +0530

    Add example for Index Out of Range exception handling

---
+++ excep4.py
@@ -0,0 +1,8 @@
+## question 4: Index Out of Range
+##try:
+##    items = ["apple", "banana", "cherry"] 
+##    x=int(input("enter a index of which you have to print a element="))
+##    print(items[x])
+##except IndexError:
+##    print("Error: Index 5 is out of range. The list has 3 items (valid indices: 0")
+##
