575ba8b268f6f9e7675029a9273b36cf03b39b71
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-15 23:01:17 +0530

    Create excep5.py

---
+++ excep5.py
@@ -0,0 +1,12 @@
+## question 5: Key Error Handling
+
+##try:
+##    capitals = {"France": "Paris", "Japan": "Tokyo", "Brazil": "Brasilia"}
+##    x=input("enter a key")
+##    y=capitals[x]
+##    print(y)
+##
+##except KeyError:
+##    print("key is not found in a dictonary")
+##    
+    
