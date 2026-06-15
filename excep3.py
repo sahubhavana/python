a81438baaad514c29b6b7663b92429c141f39182
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-15 23:00:21 +0530

    Add exception handling for file not found error

---
+++ excep3.py
@@ -0,0 +1,13 @@
+# question 3: File not found
+
+
+##import os
+##
+##try:
+##    filename=str(input("enter a file name="))
+##    f=open(r"C:\Users\DELL\Documents\filename","r")
+##    x=f.read()
+##    print(x)
+##except FileNotFoundError:
+##    print("file does not found")
+    
