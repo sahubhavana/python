0629ad60800b4eae96e32b3ee5a3dc0576e2f0a7
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-15 22:59:15 +0530

    Create excep1.py

---
+++ excep1.py
@@ -0,0 +1,10 @@
+question 1: User enters "hello" when prompted for a number
+
+try:
+##    user_input=input("enetr a number=")
+##    number=int(user_input)
+##    print("you enter a valid number")
+##except ValueError:
+##    print("That was not a valid integer. Please enter a number")
+
+
