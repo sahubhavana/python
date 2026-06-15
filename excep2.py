543cf8b6f1e6c45b91af291e149524cd8ec02d8f
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-15 22:59:46 +0530

    Create excep2.py

---
+++ excep2.py
@@ -0,0 +1,15 @@
+question 2:Create a function safe_divide(a, b) that divides a by b. If b is zero, handle the
+##ZeroDivisionError and return None instead of letting the program crash.
+
+##def safe_divide(a,b):
+##    try:
+##        print("it is  a perfect division=",a/b)
+##    except ZeroDivisionError:
+##        print("it can not  divide a number")
+##        return None
+##
+##
+##
+##
+##  
+##safe_divide(10,0)
