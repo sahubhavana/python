60b2a50567a9b8c731aad13ef8b22f18c8eea32f
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-16 23:36:59 +0530

    Create Fh5.py

---
+++ Fh5.py
@@ -0,0 +1,10 @@
+###5.program to add a new line in a file
+##
+##f1=open(r'C:\Users\DELL\Documents\firstfile1.txt','a')
+##for i in range(3):
+##    x=input()
+##    f1.write(x)
+##f1.close()
+##f1=open(r'C:\Users\DELL\Documents\firstfile1.txt','r')
+##x1=f1.read()
+##print(x1)
