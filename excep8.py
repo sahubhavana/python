de0f1948f736754e8711716ed6c9a83fec7bfb95
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-17 23:57:03 +0530

    Create excep8.py

---
+++ excep8.py
@@ -0,0 +1,7 @@
+## 8.Program to delete a specific line from a file
+##f1=open(r'C:\Users\DELL\Documents\firstfile1.txt','r')
+##x=input("enter a content of line=")
+##x1=f1.readlines()
+##if x in x1:
+##    x1.remove(x)
+##print(x1)
