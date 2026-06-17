34662670f6b08105dc4ac70b9bb8bb985897c2e8
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-17 23:55:58 +0530

    Create excep7.java

---
+++ excep7.java
@@ -0,0 +1,16 @@
+## 7.write a program to count no of word and character and no of word in a file
+
+##f1=open(r'C:\Users\DELL\Documents\firstfile1.txt','r')
+##
+##cnword=0
+##cnchar=0
+##
+##x=f1.read()
+##cnchar=len(x)
+##
+##x1=x.split(" ")
+##cnword=len(x1)
+##
+##print("no of character present in a file=",cnchar)
+##print("no of word present in a file=",cnword)
+##
