9444d90e0b0b14c288a9174a13892a966a7c2c8d
BHAVANA SAHU <bvasahu@gmail.com>
2026-07-01 14:59:43 +0530

    Add calculations for mean, median, and standard deviation

---
+++ np12.py
@@ -0,0 +1,6 @@
+#45.Compute the mean, median, and standard deviation of a NumPy array
+
+arr = np.array([10, 20, 30, 100, 200, 300])
+print("median:",np.median(arr))
+print("mean:",np.mean(arr))
+print("standardeviation:",np.std(arr))
