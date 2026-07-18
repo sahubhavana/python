68c1ba75505748323e6cc345b0a2a1fa220020b4
BHAVANA SAHU <bvasahu@gmail.com>
2026-07-18 22:41:28 +0530

    Create Numpy1.py

---
+++ Numpy1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+random_array = np.random.randint(1, 100, (3, 3))
+
+print("Random Array:\n", random_array)
+print("Maximum:", np.max(random_array))
+print("Minimum:", np.min(random_array))
