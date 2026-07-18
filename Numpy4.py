29317954571c2703069691ab98ca8afdeb13d428
BHAVANA SAHU <bvasahu@gmail.com>
2026-07-18 22:42:29 +0530

    Create Numpy4.py

---
+++ Numpy4.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+
+print("Addition:\n", a + b)
+print("Subtraction:\n", a - b)
+print("Multiplication:\n", a * b)
