719715c7ba3a9eed5a86f2f4871d19256a131811
BHAVANA SAHU <bvasahu@gmail.com>
2026-09-08 23:09:52 +0530

    Create everyword.py

---
+++ everyword.py
@@ -0,0 +1,18 @@
+# Reverse every word in a sentence
+
+##s = "hello world python"
+##
+##x = s.split()
+##x1 = []
+##
+##for a in x:
+##    reverse = ""
+
+    # Traverse the word from last character to first
+##    for i in range(len(a) - 1, -1, -1):
+##        reverse = reverse + a[i]
+##
+##    x1.append(reverse)
+##
+##print(" ".join(x1))
+
