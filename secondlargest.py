5f6e6226f42e148021a7e74bd56f593ffeffdc79
BHAVANA SAHU <bvasahu@gmail.com>
2026-09-23 23:25:15 +0530

    Create secondlargest.py

---
+++ secondlargest.py
@@ -0,0 +1,15 @@
+##L=[12,16,13,19,17]
+##l=L[0]
+##sl=L[0]
+##
+##for i in L:
+##    if(i>l):
+##        sl=l
+##        l=i
+##    elif(i>sl):
+##        sl=i
+##
+##print("largest=",l)
+##print("second largest=",sl)
+##        
+##
