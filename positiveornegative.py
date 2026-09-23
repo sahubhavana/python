0cf6ae58cb4fcda211cdf66a655fda23047136f1
BHAVANA SAHU <bvasahu@gmail.com>
2026-09-23 23:26:27 +0530

    Add positive and negative number separation logic

---
+++ positiveornegative.py
@@ -0,0 +1,14 @@
+##l=[10,20,-90,78,-78,-56,-3,0]
+##
+##lp=[]
+##ln=[]
+##
+##for i in l:
+##    if(i<0):
+##        ln.append(i)
+##
+##    else:
+##         lp.append(i)
+##    
+##print(lp)
+##print(ln)
